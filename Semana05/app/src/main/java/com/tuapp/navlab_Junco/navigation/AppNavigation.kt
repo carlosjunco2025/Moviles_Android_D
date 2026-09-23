cc096cfa2033740a246a633f8e327c7837cb0d5a
carlosjunco2025 <carlos.junco@tecsup.edu.pe>
2026-09-22 22:49:56 -0500

    armar grafo de navegacion central con NavHost y argumentos int
    - Configuracion del contenedor NavHost con el controlador NavController instanciado mediante rememberNavController().
    - Mapeo declarativo de los cuatro destinos (Home, List, Profile, Detail) asociados a sus respectivas composturas.
    - Configuracion explicita del argumento 'itemId' con NavType.IntType y valor por defecto (defaultValue = 0) para el manejo seguro de datos nulos.

---
+++ Semana05/app/src/main/java/com/tuapp/navlab_Junco/navigation/AppNavigation.kt
@@ -0,0 +1,41 @@
+package com.tuapp.navlab_Junco.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.tuapp.navlab_Junco.screens.*
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) {
+            HomeScreen(navController)
+        }
+        composable(Screen.List.route) {
+            ListScreen(navController)
+        }
+        composable(Screen.Profile.route) {
+            ProfileScreen(navController)
+        }
+        composable(
+            route = Screen.Detail.route,
+            arguments = listOf(
+                navArgument(name = "itemId") {
+                    type = NavType.IntType
+                    defaultValue = 0
+                }
+            )
+        ) { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 0
+            DetailScreen(navController, itemId)
+        }
+    }
+}
