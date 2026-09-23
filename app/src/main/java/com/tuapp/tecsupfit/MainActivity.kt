f63b478536f84adb4df7d1e9f7762dd2fb6ef837
carlosjunco2025 <carlos.junco@tecsup.edu.pe>
2026-09-23 04:50:57 -0500

    integrar el flujo completo de la aplicacion en MainActivity
    - Se inicializa el tema general TECSUPFitTheme con soporte para el modo oscuro.
    - Se establece AppNavigation como el punto de entrada principal dentro del setContent.
    - Se habilita enableEdgeToEdge para garantizar una experiencia visual moderna y adaptada a la pantalla."

---
+++ app/src/main/java/com/tuapp/tecsupfit/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.tecsupfit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.tecsupfit.navigation.AppNavigation
+import com.example.tecsupfit.ui.theme.TECSUPFitTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TECSUPFitTheme {
+                AppNavigation()
+            }
+        }
+    }
+}
